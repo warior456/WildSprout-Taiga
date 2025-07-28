3376edeb39e90d976d05e0fb08c474c2f4adea6a
gemsb <85642836+gemsb@users.noreply.github.com>
2025-07-28 16:05:02 +0200

    test

--- src/main/java/net/ugi/wildsprout_taiga/world/gen/ModFeatures.java
+++ src/main/java/net/ugi/wildsprout_taiga/world/gen/ModFeatures.java
@@ -18,13 +18,11 @@ public class ModFeatures {
     public static final Feature<DefaultFeatureConfig> FALLEN_TREE = new FallenTree(DefaultFeatureConfig.CODEC);
 
     public static void init(){
-
         Registry.register(Registries.FEATURE, WildSproutTaiga.identifier( "boulders"), BOULDERS);
         Registry.register(Registries.FEATURE, WildSproutTaiga.identifier( "rocks"), ROCKS);
         Registry.register(Registries.FEATURE, WildSproutTaiga.identifier( "fluffy_snow"), FLUFFY_SNOW);
         Registry.register(Registries.FEATURE, WildSproutTaiga.identifier( "moss"), MOSS);
         Registry.register(Registries.FEATURE, WildSproutTaiga.identifier( "fallen_tree"), FALLEN_TREE);
         Registry.register(Registries.FEATURE, WildSproutTaiga.identifier( "trees"), TREES);
-        //test
     }
 }
