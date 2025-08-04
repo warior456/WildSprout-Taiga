f2b982c57754656229918dd9885456e99ef792ba
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-08-05 00:19:00 +0200

    small trees mostly replaced with new ones

--- src/main/java/net/ugi/wildsprout_taiga/world/gen/feature/Trees.java
+++ src/main/java/net/ugi/wildsprout_taiga/world/gen/feature/Trees.java
@@ -3,6 +3,7 @@ package net.ugi.wildsprout_taiga.world.gen.feature;
 import com.mojang.serialization.Codec;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.noise.DoublePerlinNoiseSampler;
 import net.minecraft.util.math.random.CheckedRandom;
@@ -10,6 +11,7 @@ import net.minecraft.util.math.random.ChunkRandom;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
@@ -46,7 +48,7 @@ public class Trees extends Feature<DefaultFeatureConfig> {
         DoublePerlinNoiseSampler mossNoise = DoublePerlinNoiseSampler.create(chunkRandom, -5, new double[]{1});
 
         double noiseSample = mossNoise.sample(center.getX(), center.getY(), center.getZ());
-        if (0.5 < noiseSample*10 -2) return false;
+        if ((0.5 < noiseSample*10 -2) && !(structureWorldAccess.getBiome(center) == BiomeKeys.OLD_GROWTH_SPRUCE_TAIGA || structureWorldAccess.getBiome(center) == BiomeKeys.OLD_GROWTH_PINE_TAIGA)) return false;
 
         if(!structureWorldAccess.getBlockState(center.down()).isIn(ModTags.Blocks.VALID_TAIGA_GENERATE_BLOCK)) return false;
 
